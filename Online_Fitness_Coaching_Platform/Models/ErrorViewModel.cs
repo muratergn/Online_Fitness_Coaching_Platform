59babffeeda42ebd1fd3a5673fcdcb25024d83ed
Murat Ergin <muratergin415@gmail.com>
2023-11-28 12:15:40 +0300

    Add project files.

---
+++ Online_Fitness_Coaching_Platform/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Online_Fitness_Coaching_Platform.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
